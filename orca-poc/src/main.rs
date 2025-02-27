f74e4219977249891118e47a4c19af93856631e8
ForgeWeaver <200391320+ForgeWeaver@users.noreply.github.com>
2025-02-27 21:41:36 +0000

    Complete the env setup example

--- orca-poc/src/main.rs
+++ orca-poc/src/main.rs
@@ -1,8 +1,7 @@
-use solana_sdk::signer::Signer;
-use solana_sdk::signer::keypair::Keypair;
-use std::env;
-use std::fs;
-use std::path::PathBuf;
+use orca_whirlpools::{WhirlpoolsConfigInput, set_funder, set_whirlpools_config_address};
+use solana_client::rpc_client::RpcClient;
+use solana_sdk::signer::{Signer, keypair::Keypair};
+use std::{env, fs, path::PathBuf};
 
 fn get_solana_keypair_path() -> PathBuf {
     let home_dir = env::var("HOME").expect("Could not find HOME directory");
@@ -13,6 +12,7 @@ fn get_solana_keypair_path() -> PathBuf {
 
 // https://dev.orca.so/Whirlpools%20SDKs/Whirlpools/Environment%20Setup
 fn main() {
+    // Wallet Management
     let keypair_path = get_solana_keypair_path();
     println!("Solana keypair path: {:?}", keypair_path);
 
@@ -20,4 +20,18 @@ fn main() {
     let keypair_bytes: Vec<u8> = serde_json::from_str(&wallet_string).unwrap();
     let wallet = Keypair::from_bytes(&keypair_bytes).unwrap();
     println!("Valid keypair found. Public key: {:?}", wallet.pubkey());
+
+    // Configure the Whirlpools SDK for Your Network
+    println!("Connecting to SolanaDevnet...");
+    set_whirlpools_config_address(WhirlpoolsConfigInput::SolanaDevnet).unwrap();
+
+    // Airdrop SOL to Your Wallet
+    let rpc_client = RpcClient::new("https://api.devnet.solana.com");
+    match rpc_client.request_airdrop(&wallet.pubkey(), 1_000_000_000) {
+        Ok(signature) => println!("Airdrop successful. Transactoin signature: {:?}", signature),
+        Err(e) => println!("Error: {:?}", e),
+    }
+
+    // Set the default funder for Transactions
+    set_funder(wallet.pubkey()).unwrap();
 }
